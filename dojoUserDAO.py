0a5d0f697c62aa5d899ace8bdc199354a5f30fef
Michalik Adrian <Adrian.Michalik@coi.gov.pl>
2015-12-12 19:58:01 +0100

    added dojo_user DAO

---
+++ dojoUserDAO.py
@@ -0,0 +1,26 @@
+import dbConnection
+
+
+def add_user_dojo_relation(userId, dojoId):
+    conn = dbConnection.connect_to_database()
+    params = (dojoId, userId)
+    if conn.execute("INSERT INTO dojo_user VALUES (?, ?)", params):
+        ilosc_mentorow = conn.execute("SELECT IL_MENTOROW FROM dojo WHERE ID = ?", dojoId)
+        ilosc_mentorow += 1
+        parameters = (ilosc_mentorow, dojoId)
+        if conn.execute("UPDATE dojo SET IL_MENTOROW = ? WHERE ID = ?", parameters):
+            dbConnection.commit(conn)
+            return True
+    else:
+        dbConnection.rollback(conn)
+        return False
+
+
+def remove_user_dojo_relation(userId, dojoId):
+    conn = dbConnection.connect_to_database()
+    if conn.execute("DELETE FROM dojo_user WHERE USER_ID = ? AND DOJO_ID = ?", userId, dojoId):
+        dbConnection.commit(conn)
+        return True
+    else:
+        dbConnection.rollback(conn)
+        return False
