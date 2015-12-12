90c09ee4eeaae1f5855450dd6ec502044253638f
Michalik Adrian <Adrian.Michalik@coi.gov.pl>
2015-12-12 22:38:37 +0100

    better handling user adding

--- userDAO.py
+++ userDAO.py
@@ -1,15 +1,31 @@
 import dbConnection
 
 
-def add_user():
+def add_user(status, login, email, password, passwordRepeat, skillsList, remoteExperienceList, miejscowosc):
     conn = dbConnection.connect_to_database()
-    if conn.execute("INSERT INTO user (EMAIL, PASSWORD, LOGIN, LOKLIZACJA, CZY_USPIONY, CZY_ZDALNY, CZY_PEDAGOG) "
-                 "VALUES ('adrian@a.pl', 'aa', 'aa', 'bb', '1', '1', '1' )"):
-        dbConnection.commit(conn)
-        return True
+    czyZdalny = 0
+    czyPedagog = 0
+    if status is "join":
+        czyUspiony = 0
+    elif status is "sleep":
+        czyUspiony = 1
     else:
-        dbConnection.rollback(conn)
         return False
+    if password is passwordRepeat:
+        if "remote" in remoteExperienceList:
+            czyZdalny = 1
+        elif "experience" in remoteExperienceList:
+            czyPedagog = 1
+        else:
+            return False
+        params = (email, password, login, miejscowosc, czyUspiony, czyZdalny, czyPedagog)
+        if conn.execute("INSERT INTO user (EMAIL, PASSWORD, LOGIN, LOKLIZACJA, CZY_USPIONY, CZY_ZDALNY, CZY_PEDAGOG) "
+                     "VALUES (?, ?, ?, ?, ?, ?, ?)", params):
+            dbConnection.commit(conn)
+            return True
+        else:
+            dbConnection.rollback(conn)
+            return False
 
 
 def does_user_exist(login, password):
--- userUmiejetnoscDAO.py
+++ userUmiejetnoscDAO.py
@@ -1,16 +1,11 @@
 import dbConnection
 
 
-def add_relations(userId, umiejetnosciIds):
-    conn = dbConnection.connect_to_database()
-    for id in umiejetnosciIds:
-        params = (userId, id)
-        if conn.execute("INSERT INTO user_umiejetnosc VALUES (?, ?)", params):
-            dbConnection.commit(conn)
-            return True
-        else:
-            dbConnection.rollback(conn)
-            return False
+def add_relations(userId, umiejetnosciNames, conn):
+    for name in umiejetnosciNames:
+        params = (userId, name)
+        conn.execute("INSERT INTO user_umiejetnosc VALUES (?, ?)", params)
+        dbConnection.commit(conn)
 
 
 def delete_users_umiejetnosci(userId):
