c7f6d50df873fce8719e672bc8e1a4a2876189b6
Michalik Adrian <Adrian.Michalik@coi.gov.pl>
2015-12-13 14:47:42 +0100

    better registration

--- rejestracjaController.py
+++ rejestracjaController.py
@@ -3,23 +3,33 @@ import userDAO
 
 def rejestruj_uzytkownika_controller(form):
     if form['password'][0] == form['passwordRepeat'][0]:
+        if 'undefined' in dict(form).keys():
+            del form['undefined']
         sleep = 1
         if form['registerMode'][0] == "join":
+            del form['registerMode']
             sleep = 0
         login = form['login'][0]
+        del form['login']
         email = form['email'][0]
+        del form['email']
         password = form['password'][0]
+        del form['password']
+        del form['passwordRepeat']
         remote = 0
-        if "remo
+        if 'remote' in dict(form).keys():
+            del form['remote']
+            remote = 1
         czyPedagog = 0
-        # if len(form[6]) > 0:
-        #     if "remote" in form[6]:
-        #         remote  = 1
-        #     if "experience" in form[6]:
-        #         czyPedagog = 1
+        if 'experience' in dict(form).keys():
+            del form['experience']
+            czyPedagog = 1
+        skillsTuple = ()
+        for key in form:
+            skillsTuple = skillsTuple + (key,)
         # miejscowosc = form[7]
         # if userDAO.add_user(login, email, password, skillsList, remote, czyPedagog, sleep, miejscowosc):
-        if userDAO.add_user(login, email, password, ('skill1',), remote, czyPedagog, sleep, "Warszawa"):
+        if userDAO.add_user(login, email, password, skillsTuple, remote, czyPedagog, sleep, "Warszawa"):
             return True
     else:
         return False
