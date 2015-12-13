c786eee81490ecb6f5540c019c945603b90e3f80
Jakub Walczak <jakub.s.walczak@gmail.com>
2015-12-13 15:41:54 +0100

    wylogowywanie

---
+++ front/dist/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$(".message-new").click(function () {
+	    $(this).removeClass("message-new");
+	});
+
+	$("#registerForm").submit(function (event) {
+		event.preventDefault();
+
+		var output = $(this).serializeArray();
+
+		//Umiejętności
+		var skills = new Array();		
+		$.each($(".skills input:checked"), function() {
+		  skills.push($(this).val());
+		});
+		output.push(skills);
+
+		//Dodatkowe
+		var info = new Array();
+		$.each($(".info input:checked"), function() {
+		  info.push($(this).val());
+		});
+		output.push(info);
+
+		console.log(output);
+
+		$.post('/register', output);
+	});
+
+/*	$("#loginForm").submit(function (event) {
+		event.preventDefault();
+
+		var output = $(this).serializeArray();
+
+		console.log(output);
+	});*/
+});
